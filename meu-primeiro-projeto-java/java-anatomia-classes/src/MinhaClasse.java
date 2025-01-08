d93bfa13642cdd50f0696feb3d0176a9a49f03ba
lucas.fonseca <lucas.fonseca@superbid.net>
2025-01-07 22:00:52 -0300

    Criação do diretório de Anatomia de Classes

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MinhaClasse {
+    
+    //Bloco de código
+
+    
+    public static void main (String [] args) {
+        System.out.println("Meu primeiro código Java");
+        System.out.println("");
+        System.out.println("Qual o seu nome?");
+        
+        Scanner scanner = new Scanner(System.in);
+        String nome = scanner.nextLine();
+
+        System.out.println("Seu nome é " + nome);
+
+        scanner.close();
+    }
+
+
+}
