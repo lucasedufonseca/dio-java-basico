a7589d423ff5e7d14bc0aa131c90ccc49a27e7e0
lucas.fonseca <lucas.fonseca@superbid.net>
2025-01-21 21:38:53 -0300

    End of tipos de variáveis project

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double monthSalary = 4500.50;
+        String employeeName = "Carlos Piloto";
+        int ageOfTheEmployee = 18;
+        double riseSalaryValue = 2500.34;
+        double newSalary = monthSalary + riseSalaryValue;
+
+
+        System.out.println("The employee "+ employeeName +" have "+ ageOfTheEmployee + " years old and earns "+ monthSalary +" by month.");
+        System.out.println(newSalary);
+
+        //Changeble variables
+        int randonNumber = 23;
+        System.out.println(randonNumber);
+        
+        randonNumber = 34;
+        System.out.println(randonNumber);
+
+        //constant
+
+        final int ONE_HUNDRED = 100;
+        System.out.println(ONE_HUNDRED);
     }
 }
